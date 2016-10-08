b1cbc6b00b8b32e4129ecc69a5c08baaf6515f6d
plabadille <plabadille@gmail.com>
2016-10-08 21:18:31 +0200

    ajout crawler fb et du site pour les dates de match

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
 sys.path.insert(0, 'crawlers')
 import crawlerTwitter
+import crawlerFacebook
+import crawlerMatchNfo
 
-
-crawlerTwitter.crawlTweets('#SMCaen','2016-10-01','2016-10-03','data.json')
+crawlerTwitter.crawlTweets('#SMCaen','2016-10-01','2016-10-03','data_twitter.json')
+crawlerFacebook.crawlFacebook('SMCaen.officiel','2016:09:16 08:00:00','2016:09:17 23:59:59','data_facebook.json')
+crawlerMatchNfo.crawlMatchDate('matchNfo.txt')
