7c155b876034ad03ad6772e993249351e85375d8
Couellan Lenaic <21201323@clow64.etu.info.unicaen.fr>
2016-10-10 12:38:54 +0200

    Modification de la BDD

--- db/db.py
+++ db/db.py
@@ -4,13 +4,32 @@ import sys
 sys.path.insert(0, 'config')
 import config #app token, not gitable
 
-def insertDB(database,collection,filename):
+def connect(database):
+	client = MongoClient('mongodb://'+ config.MONGO_USER +':'+ config.MONGO_PASS +'@mongodb.info.unicaen.fr:27017/')
+	db = client[database]
+	return db
 
-    client = MongoClient('mongodb://'+ config.MONGO_USER +':'+ config.MONGO_PASS +'@mongodb.info.unicaen.fr:27017/')
-    db = client[database]
+def insertDB(db,collection,filename):
+
     col = db[collection]
     data = []
     with open("json/"+filename) as f:
         for line in f:
             data.append(json.loads(line))
     col.insert(data)
+
+
+def find(collection, query , colAffichees):
+	posts = []
+	for post in collection.find( query , colAffichees ):
+		posts.append(str(post["text"]))
+	return posts
+
+def findAll(collection):
+	posts = []
+	for post in collection.find():
+		posts.append(post)
+	return posts
+
+def deleteData(collection):
+	collection.delete_many({})
