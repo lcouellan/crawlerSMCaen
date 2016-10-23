834c168249dfc9147d3e8d573fa18b775acdc8c6
Labadille Pierre <21101555@clow64.etu.info.unicaen.fr>
2016-10-23 15:51:43 +0200

    add a dependencies install shell script

---
+++ installDependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#auto install of dependencies
+pip3 install pymongo
+pip3 install twitter
+pip3 install requests
+echo "done installing dependencies pymongo, twitter, requests"
+echo "if it doesn't work, please install python3-pip"
+echo "note that this app need python3 in order to work"
