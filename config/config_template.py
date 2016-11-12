d78115eb5a5cb07fac17a2c66b459f4a4816e4db
Lénaïc Couëllan <lcouellan@gmail.com>
2016-11-12 15:24:22 +0100

    Create config_template.py

---
+++ config/config_template.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+#Twitter
+TOKEN = ""
+TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+
+#facebook
+APP_ID = ""
+APP_SECRET_KEY = ""
+
+# Mongo
+MONGO_USER = ""
+MONGO_PASS = ""
+MONGO_DB = ""
